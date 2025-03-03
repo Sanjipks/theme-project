df131e9120510f4a4a85cb076ad8dafb0173935b
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2025-03-03 11:56:15 -0500

    progress on theme work

--- app/fixedComponents/themeSelect.tsx
+++ app/fixedComponents/themeSelect.tsx
@@ -1,6 +1,13 @@
 import { useState, useEffect } from "react";
 
-const themes = ["light", "dark", "blue", "green", "yellow", "red"];
+const themes = [
+  { name: "light", color: "white" },
+  { name: "dark", color: "black" },
+  { name: "blue", color: "blue" },
+  { name: "green", color: "green" },
+  { name: "yellow", color: "yellow" },
+  { name: "red", color: "red" },
+];
 
 const ThemeSelect = () => {
   const [theme, setTheme] = useState<string>(() => {
@@ -15,27 +22,59 @@ const ThemeSelect = () => {
     }
     return "light";
   });
+  const [openThemeOptions, setOpenThemeOptions] = useState(false);
 
   useEffect(() => {
     document.body.className = theme;
     localStorage.setItem("theme", theme);
   }, [theme]);
 
-  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>): void => {
-    setTheme(event.target.value);
+  const handleSelectTheme = (selectedtheme: string): void => {
+    setTheme(selectedtheme);
+  };
+
+  const handleShowThemeOptions = (): void => {
+    setOpenThemeOptions((prev) => !prev);
   };
 
   return (
-    <label className=" flex md:absolute relative h-10 flex-col bg-amber-50  mx-auto   cursor-pointer ">
-      <p className="bg-amber-800">Select Theme</p>
-      <select value={theme} onChange={handleChange} className="form-select">
-        {themes.map((themeOption) => (
-          <option key={themeOption} value={themeOption}>
-            {themeOption.charAt(0).toUpperCase() + themeOption.slice(1)}
-          </option>
-        ))}
-      </select>
-    </label>
+    <div className=" p-4">
+      <label className=" flex absolute flex-col bg-amber-50  cursor-pointer ">
+        <div className="flex flex-col gap-0">
+          <p className="bg-amber-400">Select Theme</p>
+          <div
+            className={`flex h-6 w-auto ${
+              theme === "dark"
+                ? "bg-gray-800"
+                : theme === "light"
+                ? "bg-gray-200"
+                : `bg-${theme}-800`
+            } rounded-full`}
+            onClick={handleShowThemeOptions}
+          ></div>
+        </div>
+        {openThemeOptions && (
+          <div className="w-auto h-auto bg-white">
+            {themes.map((themeOption) => (
+              <div
+                key={themeOption.name}
+                className={`flex w-auto h-6 relative rounded-full `}
+                onClick={() => handleSelectTheme(themeOption.name)}
+              >
+                <div>
+                  {" "}
+                  <span>
+                    {themeOption.name.charAt(0).toUpperCase() +
+                      themeOption.name.slice(1)}
+                  </span>
+                  <div className={`flex b h-6 w-6`}></div>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+      </label>
+    </div>
   );
 };
 
