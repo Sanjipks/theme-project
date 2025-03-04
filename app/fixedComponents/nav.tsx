ec52d0ff510502ae82acf9c82fc0edfa06d935b8
Sanjip K Sapkota <sapkotasanjipk@outlook.com>
2025-03-04 11:48:22 -0500

    make theme color selection more distinguishable

--- app/fixedComponents/nav.tsx
+++ app/fixedComponents/nav.tsx
@@ -4,7 +4,7 @@ type Props = {};
 
 const Navbar = (props: Props) => {
   return (
-    <div className="h-20 w-full bg-amber-300 ">
+    <div className="h-20 w-full bg-gray-200 dark:bg-gray-900 dark:text-gray-100 blue:text-gray-100 green:text-gray-100 red:text-gray-100 blue:bg-blue-600 green:bg-green-600 yellow:bg-yellow-600 red:bg-red-600  ">
       <ThemeSelect />
     </div>
   );
